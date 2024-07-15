e648351ab7c3cf002dcf8efae1dcebecb6d0550a
chrstief <chrstief@gmail.com>
2024-07-15 23:06:19 +0200

    remove ruler

--- init.lua
+++ init.lua
@@ -163,6 +163,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Deactivate status bar
 vim.opt.laststatus = 0
+vim.opt.ruler = false
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
