c18c8e2dc72d90a935c93e387aef3b8413c81de5
chrstief <chrstief@gmail.com>
2024-07-16 21:08:51 +0200

    add gleam to treesitter

--- init.lua
+++ init.lua
@@ -789,7 +789,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript' },
+      ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript', 'gleam' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
