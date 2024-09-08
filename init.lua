c198227cb3af46d6fbea4693eb3ce820353ff72c
chrstief <chrstief@gmail.com>
2024-09-08 22:39:48 +0200

    merge format autocmds

--- init.lua
+++ init.lua
@@ -921,12 +921,7 @@ require('lazy').setup({
 -- Create an autocommand to format on:
 -- InsertLeave: when leaving Insert mode
 -- TextChanged: after a change was made to the text in Normal mode
-vim.api.nvim_create_autocmd('InsertLeave', {
-  callback = function()
-    require('conform').format { async = true }
-  end,
-})
-vim.api.nvim_create_autocmd('TextChanged', {
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
   callback = function()
     require('conform').format { async = true }
   end,
