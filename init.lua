500acc28c5191c9a83157d3ca2c16b72648f20ef
chrstief <chrstief@gmail.com>
2024-09-10 16:50:21 +0200

    move my custom options next to the original options

--- init.lua
+++ init.lua
@@ -157,17 +157,17 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Deactivate status bar
+vim.opt.laststatus = 0
+vim.opt.ruler = false
+vim.opt.cmdheight = 0
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Deactivate status bar
-vim.opt.laststatus = 0
-vim.opt.ruler = false
-vim.opt.cmdheight = 0
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
