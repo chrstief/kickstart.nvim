ed4d14baaebe00837387882162c1f4caf186b815
chrstief <chrstief@gmail.com>
2024-10-07 13:17:56 +0200

    replace ts_ls with typescript-tools

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,8 +3,27 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { -- Auto close HTML Tags
-    'windwp/nvim-ts-autotag',
+  -- { -- Auto close HTML Tags
+  --   'windwp/nvim-ts-autotag',
+  --   opts = {},
+  -- },
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
+    config = function()
+      require('typescript-tools').setup {
+        on_attach = function(client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        settings = {
+          jsx_close_tag = {
+            enable = true,
+            filetypes = { 'javascriptreact', 'typescriptreact' },
+          },
+        },
+      }
+    end,
   },
 }
